b32ace213cd3c437e990579b3e3713cc5b2456ac
MIraK1E <mike.pattana@gmail.com>
2017-12-06 01:04:49 +0700

    Fix controller name

--- controllers/users.php
+++ controllers/users.php
@@ -1,8 +1,12 @@
 <?php
 
-    class User extends Controller
+    class Users extends Controller
     {
-        
+        protected function login()
+        {
+            $viewmodal = new ShareModel();
+            $this->returnView($viewmodal->Index(),true);
+        }
     }
 
 ?>
