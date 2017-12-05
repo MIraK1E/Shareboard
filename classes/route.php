0fec288814124cb2093f499837af48ebf80c9b57
MIraK1E <mike.pattana@gmail.com>
2017-12-05 23:11:39 +0700

    fix undefined method

--- classes/route.php
+++ classes/route.php
@@ -15,15 +15,51 @@
             }
             else
             {
-                $this->request = $this->request['controller'];
+                $this->controller = $this->request['controller'];
             }
             if($this->request['action'] == "")
             {
-                $this->request = 'index.php'
+                $this->action = 'index';
             }
             else
             {
-                $this->request = $this->request['action'];
+                $this->action = $this->request['action'];
+            }
+
+        }
+
+        public function createController()
+        {
+            //Check Class
+            if(class_exists($this->controller))
+            {
+                $parents = class_parents($this->controller);
+                //check Extend
+                if(in_array("Controller", $parents))
+                {
+                    if(method_exists($this->controller, $this->action))
+                    {
+                        return new $this->controller($this->action, $this->request);
+                    }
+                    else
+                    {
+                        // Method Does Not Exist
+                        echo '<h1>Method does not exist</h1>';
+                        return;
+                    }
+                }
+                else
+                {
+                    // Base Controller Does Not Exist
+                    echo '<h1>Base Controller does not exist</h1>';
+                    return;
+                }
+            }
+            else
+            {
+                // Controller Class Does Not Exist
+                echo '<h1>Controller Class does not exist</h1>';
+                return;
             }
         }
     }
