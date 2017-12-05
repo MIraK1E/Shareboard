1ea41afbd45f9e2657c1c165a1b0cd2496578fbe
MIraK1E <mike.pattana@gmail.com>
2017-12-05 23:18:11 +0700

    require user, shares controller

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     require('classes/Controller.php');
 
     require('controllers/home.php');
+    require('controllers/shares.php');
+    require('controllers/users.php');
 
     $route = new Route($_GET);
     $controller = $route->createController();
