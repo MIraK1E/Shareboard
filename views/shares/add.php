2eaeaa3f266bbb727f68e012ab28441bf60eb30a
MIraK1E <mike.pattana@gmail.com>
2017-12-06 02:13:57 +0700

    create add form

---
+++ views/shares/add.php
@@ -0,0 +1,19 @@
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Share Somthing!</h3>
+  </div>
+  <div class="panel-body">
+    <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+        <div class="form-group">
+            <label>Share Title</label>
+            <input type="text" name="title" class="form-control">
+            <label>Body</label>
+            <textarea name="body" class="form-control"></textarea>
+            <label>Link</label>
+            <input type="text" name="link" class="form-control">
+        </div>
+            <input type="submit" name="submit" value="submit" class="btn btn-primary">
+            <a class="btn btn-danger" href="<?= ROOT_PATH ?>shares">Cancle</a>
+    </form>
+  </div>
+</div>
