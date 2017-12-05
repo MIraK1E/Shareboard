67cb5571890023da321244820d0c22903657703d
MIraK1E <mike.pattana@gmail.com>
2017-12-05 23:57:07 +0700

    create modal and set config file to work with database

---
+++ classes/Model.php
@@ -0,0 +1,57 @@
+<?php
+
+    abstract class Model
+    {
+        protected $databaseHandle;
+        protected $statement;
+
+        public function __construct()
+        {
+            $this->databaseHandle = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PASS);
+        }
+
+        public function query($query)
+        {
+            $this->statement = $this->databaseHandle->prepare($query);
+        }
+
+        public function bind($param, $value, $type = null)
+        {
+            if(is_null($type))
+            {
+                switch(true)
+                {
+                    case is_int($value):
+                        $type = PDO::PARAM_INT;
+                        break;
+
+                    case is_bool($value):
+                        $type = PDO::PARAM_BOOL;
+                        break;
+
+                    case is_null($value):
+                        $type = PDO::PARAM_NULL;
+                        break;
+
+                    default:
+                        $type = PDO::PARAM_STR;
+
+                }
+            }
+            $this->statement->bindValue($param, $value, $type);
+        }
+
+        public function execute()
+        {
+            return $this->statement->execute();
+        }
+
+        public function resultset()
+        {
+            $this->execute();
+            return $this->statement->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+    }
+
+?>
---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+//Define DB Param
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("DB_NAME", "shareboard");
+
+//Define URL
+    define("ROOT_PATH", "/");
+    define("ROOT_URL", "http://localhost/code/Shareboard");
+?>
