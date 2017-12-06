50d72b1b8fc1cd499117ebfdeb7f214b026e5463
MIraK1E <mike.pattana@gmail.com>
2017-12-06 23:31:52 +0700

    add function login logout

--- controllers/users.php
+++ controllers/users.php
@@ -2,10 +2,25 @@
 
     class Users extends Controller
     {
-        protected function login()
+        protected function register ()
         {
-            $viewmodal = new ShareModel();
-            $this->returnView($viewmodal->Index(),true);
+            $viewmodel = new UserModel();
+            $this->returnView($viewmodel->register(),true);
+        }
+
+        protected function login ()
+        {
+            $viewmodel = new UserModel();
+            $this->returnView($viewmodel->login(),true);
+        }
+
+        protected function logout ()
+        {
+            unset($_SESSION['is_logged_in']);
+            unset($_SESSION['user_data']);
+            session_destroy();
+
+            header('Location:'.ROOT_URL);
         }
     }
 
